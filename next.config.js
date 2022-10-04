6427629e4408d5bdf11523a0e3c8fd234642cf7c
Felipe Román <froman@stratechcorp.com>
2022-10-04 17:08:21 -0300

    add ignoreDuringBuils typescript

--- next.config.js
+++ next.config.js
@@ -1,6 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  eslint: {
+    dirs: ['.'],
+    ignoreDuringBuilds: true,
+  },
 };
 
 module.exports = nextConfig;
